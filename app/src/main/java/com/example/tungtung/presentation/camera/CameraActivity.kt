211e4bb4aea40672aae8e5aaf6c32e451abc92ee
josephmagara <joseph.magara16@gmail.com>
2020-10-09 11:26:39 +1100

    Added permission handling for the camera permissions.

--- app/src/main/java/com/example/tungtung/presentation/camera/CameraActivity.kt
+++ app/src/main/java/com/example/tungtung/presentation/camera/CameraActivity.kt
@@ -1,13 +1,40 @@
 package com.example.tungtung.presentation.camera
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.tungtung.R
 
 class CameraActivity : AppCompatActivity() {
 
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+
+    lateinit var cameraViewModel: CameraViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cameraViewModel.openCamera().observe(this, {
+            startCamera()
+        })
+
+        cameraViewModel.onPermissionNotGranted().observe(this, {
+            Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show()
+            finish()
+        })
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            cameraViewModel.permissionRequestCompleted()
+        }
+    }
+
+    private fun startCamera() {
+
     }
 }
--- app/src/main/java/com/example/tungtung/presentation/camera/CameraViewModel.kt
+++ app/src/main/java/com/example/tungtung/presentation/camera/CameraViewModel.kt
@@ -1,6 +1,9 @@
 package com.example.tungtung.presentation.camera
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.tungtung.data.utils.PermissionsHelper
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -8,13 +11,27 @@ import java.util.concurrent.Executors
  * Created by josephmagara on 9/10/20.
  */
 
-class CameraViewModel : ViewModel() {
+class CameraViewModel(private val permissionsHelper: PermissionsHelper) : ViewModel() {
 
     private var cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+
+    private val openCamera = MutableLiveData<Any>()
+    private val permissionsNotGranted: MutableLiveData<Any> = MutableLiveData<Any>()
 
     override fun onCleared() {
         super.onCleared()
         cameraExecutor.shutdown()
     }
 
+    fun permissionRequestCompleted() {
+        if (permissionsHelper.allCameraPermissionsGranted()) {
+            openCamera.value = true
+        } else {
+            permissionsNotGranted.value = true
+        }
+    }
+
+    fun openCamera(): LiveData<Any> = openCamera
+
+    fun onPermissionNotGranted(): LiveData<Any> = permissionsNotGranted
 }
