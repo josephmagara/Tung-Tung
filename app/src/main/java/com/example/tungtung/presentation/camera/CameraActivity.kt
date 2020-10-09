4295cb2d373d55a450640a581a5508d578e0ab55
josephmagara <joseph.magara16@gmail.com>
2020-10-09 15:42:15 +1100

    Got the camera to work.

--- app/src/main/java/com/example/tungtung/presentation/camera/CameraActivity.kt
+++ app/src/main/java/com/example/tungtung/presentation/camera/CameraActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import com.example.tungtung.R
 import com.example.tungtung.data.utils.PermissionsHelper.Companion.REQUEST_CODE_PERMISSIONS
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+import kotlinx.android.synthetic.main.camera_view.*
+import timber.log.Timber
 
 @AndroidEntryPoint
 class CameraActivity : AppCompatActivity() {
@@ -28,14 +33,43 @@ class CameraActivity : AppCompatActivity() {
         })
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
-                                            grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>,
+        grantResults: IntArray
+    ) {
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             cameraViewModel.permissionRequestCompleted()
         }
     }
 
     private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(camera_view.surfaceProvider)
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+
+            } catch (exc: Exception) {
+                Timber.e("Use case binding failed: + $exc")
+            }
+
+        }, ContextCompat.getMainExecutor(this))
     }
 }
