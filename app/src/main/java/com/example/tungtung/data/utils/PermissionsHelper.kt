6d5610880f479ec368b2d8153b020f0769b94a74
josephmagara <joseph.magara16@gmail.com>
2020-10-09 15:03:13 +1100

    Got the requesting of permissions to work.

--- app/src/main/java/com/example/tungtung/data/utils/PermissionsHelper.kt
+++ app/src/main/java/com/example/tungtung/data/utils/PermissionsHelper.kt
@@ -2,14 +2,13 @@ package com.example.tungtung.data.utils
 
 import android.Manifest
 import android.app.Activity
-import android.app.ActivityManager
 import android.app.Application
-import android.content.ComponentName
 import android.content.Context
+import android.content.ContextWrapper
 import android.content.pm.PackageManager
-import android.os.Build
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.example.tungtung.application.TungTungApplication
 import javax.inject.Inject
 
 
@@ -21,24 +20,33 @@ class PermissionsHelper @Inject constructor(private val context: Context) {
     companion object {
         const val REQUEST_CODE_PERMISSIONS = 10
     }
+
     private val requiredCameraPermissions = arrayOf(Manifest.permission.CAMERA)
 
     fun allCameraPermissionsGranted() = requiredCameraPermissions.all {
         ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
     }
 
-    fun requestCameraPermissions(){
-/*        ActivityCompat.requestPermissions(
-            context, requiredCameraPermissions, REQUEST_CODE_PERMISSIONS
-        )*/
-    }
-/*
-    private fun getCurrentActivity(): Activity {
-        val am = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            am.appTasks[0].taskInfo.topActivity
-        } else {
-            am.getRunningTasks(1)[0].topActivity
+    fun requestCameraPermissions() {
+        getActivityFromContext(context)?.let {
+            ActivityCompat.requestPermissions(it, requiredCameraPermissions, REQUEST_CODE_PERMISSIONS)
         }
-    }*/
+    }
+
+    private fun getActivityFromContext(context: Context): Activity? {
+        if (context is ContextWrapper) {
+            return when (context) {
+                is Activity -> {
+                    context
+                }
+                is Application -> {
+                    (context as TungTungApplication).getActiveActivity()
+                }
+                else -> {
+                    getActivityFromContext(context.baseContext)
+                }
+            }
+        }
+        return null
+    }
 }
